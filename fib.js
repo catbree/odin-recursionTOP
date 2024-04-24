7b8ec7f4cc9ad4b2079b806e615587b5feb6f738
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-04-24 19:42:05 +0800

    tackled fibonacci recursively... took awhile and some chatgpt (,:

--- fib.js
+++ fib.js
@@ -1,3 +1,6 @@
+//solves fibonacci sequence using iteration
+console.log('Start: FIBONACCI ITERATIVE')
+
 function fibs(n) {
 
     let array = [];
@@ -26,3 +29,27 @@ function fibs(n) {
 fibs(1);
 fibs(2);
 fibs(5);
+
+//solves fibonacci sequence using recursion
+
+console.log('Start: FIBONACCI ITERATIVE')
+
+function fibRec(n) {
+    if (n === 0 ) {
+        return [];
+    } else if (n === 1) {
+        return [0]
+    } else if (n === 2) {
+        return [0,1]
+    } else {
+        var arr = fibRec(n-1);
+        arr.push(arr[arr.length-1] + arr[arr.length-2]) //using arr.length instead of n here as n indicates desired length of sequence and not current length of sequence being built. 
+        return arr;
+    }
+}
+
+console.log(fibRec(4))
+console.log(fibRec(8))
+
+
+
